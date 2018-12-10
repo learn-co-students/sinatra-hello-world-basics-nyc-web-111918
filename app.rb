4bb81199bac32dc1666ad3f8ed3a97fd98a03cc5
isaac <oseasisaac91@gmail.com>
2018-12-10 09:57:52 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+  # def call (env)
+  #
+  # end
+
+  get '/' do
+    "Hello, World!"
+  end
 
 
 end
